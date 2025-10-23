962f6ee95a66236deda5008e2012a5795b6d1b6c
Nick <unitflash79@yandex.ru>
2025-10-23 20:09:34 +0300

    OO_DimmerSwitch_Model1

---
+++ Chapter1/G3/DimmerSwitch_Model2_Method_Calls.py
@@ -0,0 +1,13 @@
+from DimmerSwitch import *
+
+# создаем два объекта DimmerSwitch
+
+o_dimmer1 = DimmerSwitch('Dimmer1')
+o_dimmer2 = DimmerSwitch('Dimmer2')
+
+# просим oDimmer1 поднять свой уровень яркости
+o_dimmer1.raise_level()
+
+# просим o_dimmer2 поднять свой уровень яркости
+o_dimmer2.raise_level()
+
---
+++ Chapter1/G3/OO_DimmerSwitch_Model2_Instantiation.py
@@ -0,0 +1,15 @@
+from DimmerSwitch import *
+o_dimmer1 = DimmerSwitch('Dimmer1')
+print(type(o_dimmer1))
+print(o_dimmer1)
+print()
+
+o_dimmer2 = DimmerSwitch('Dimmer2')
+print(type(o_dimmer2))
+print(o_dimmer2)
+print()
+
+o_dimmer3 = DimmerSwitch('Dimmer3')
+print(type(o_dimmer3))
+print(o_dimmer3)
+print()
